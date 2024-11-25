b72ccec6b04673d58a030ee96fc8fcb5d050f6e5
Yana Rozova <rozova.ys@gmail.com>
2024-11-25 18:32:57 +0300

    JS animation started

--- js/modules/animation.js
+++ js/modules/animation.js
@@ -19,5 +19,29 @@
 var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 console.log('JS in work');
+const Width = {
+  DESKTOP_WIDTH: 1920,
+  TABLET_WIDTH: 768,
+  MOBILE_WIDTH: 320,
+  CONTAINER_WIDTH: 1440
+};
+const windowWidth = document.documentElement.clientWidth;
+
+//---BECOME---
+
+const sliderBecome = document.querySelector('.become__slides');
+const toggleClasses = () => {
+  sliderBecome.classList.toggle('become__slides--active-right');
+};
+const setMobileAnimation = () => {
+  let animationTimeout = null;
+  if (windowWidth < Width.TABLET_WIDTH) {
+    animationTimeout = setInterval(toggleClasses, 3500);
+  } else {
+    sliderBecome.classList.contains('become__slides--active-right') && sliderBecome.classList.remove('become__slides--active-right');
+    animationTimeout && clearInterval(animationTimeout);
+  }
+};
+setMobileAnimation();
 /******/ })()
 ;
