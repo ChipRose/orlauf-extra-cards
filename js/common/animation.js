0832a7aed8c3be8a23895f34563829aaf8923515
Yana Rozova <rozova.ys@gmail.com>
2024-11-26 15:01:50 +0300

    Start block with custom cursor

--- js/common/animation.js
+++ js/common/animation.js
@@ -33,3 +33,36 @@ window.addEventListener('resize', () => {
   setAnimation();
 });
 
+//---CURSOR---
+
+const cursor = document.getElementById('cursor');
+const cursorArea = document.getElementById('cursorArea');
+
+const handleMouseMove = (evt) => {
+  const x = evt.clientX;
+  const y = evt.clientY;
+
+  const rect = cursorArea.getBoundingClientRect();
+
+  const isInside = x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom;
+
+  if (isInside) {
+    cursor.style.left = `${x - 40}px`;
+    cursor.style.top = `${y - rect.top - 10}px`;
+    cursor.style.display = 'block';
+  } else {
+    cursor.style.display = 'none';
+  }
+};
+
+document.addEventListener('mousemove', handleMouseMove);
+
+cursorArea.addEventListener('mouseenter', () => {
+  document.body.style.cursor = 'none';
+});
+
+cursorArea.addEventListener('mouseleave', () => {
+  document.body.style.cursor = 'default';
+  cursor.style.display = 'none';
+});
+
